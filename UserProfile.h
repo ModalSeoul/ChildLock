2766d4614a8f9ee1ba024a2014c00d5587b116d1
ModalSeoul <algernon@teknik.io>
2016-09-12 09:24:58 -0700

    Fuck you too, Visual Studio

--- UserProfile.h
+++ UserProfile.h
@@ -19,8 +19,8 @@ class UserProfile {
 protected:
 	// To be replaced by SQL table to store children
 	vector<char*> CHILDREN = {
-		"Angeles Tagle",	// "Koze" - wrote a shotbot
-		"Mike",				// In this corner, weighing 342 lbs, 2-time felon, 1mike1!
+		"Angeles Tagle", // "Koze" - wrote a shotbot
+		"Mike",	         // In this corner, weighing 342 lbs, 2-time felon, 1mike1!
 	};
 	// Compare to 0, 1, 2 values from SQL table
 	enum h_Severity {
