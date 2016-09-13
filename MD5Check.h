41f8ebbea6202b3a6052ada54ee61c7b1e413324
ModalSeoul <algernon@teknik.io>
2016-09-12 17:09:10 -0700

    Going home

---
+++ MD5Check.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#ifndef MD5CHECK_H
+#define MD5CHECK_H
+
+class MD5Check {
+protected:
+	vector<char*> Hashes = {};
+};
+#endif
--- UserProfile.h
+++ UserProfile.h
@@ -15,12 +15,17 @@ using namespace std;
 ////////////////////////////////////////////
 
 // TODO: Implement Caesar Cipher encryption
+// to prevent string searching.
+// REMEMBER:
+//    Call encrypt() and store value in pointer, use pointer
+//    to make comparison, immediately delete[] the pointer afterwards.
+//    Failure to do this will make the entire function moot.
 class UserProfile {
 protected:
 	// To be replaced by SQL table to store children
 	vector<char*> CHILDREN = {
 		"Angeles Tagle", // "Koze" - wrote a shotbot
-		"Mike",	         // In this corner, weighing 342 lbs, 2-time felon, 1mike1!
+		"Mike",          // Mike - wrote packet dropper and various mem edits.
 	};
 	// Compare to 0, 1, 2 values from SQL table
 	enum h_Severity {
